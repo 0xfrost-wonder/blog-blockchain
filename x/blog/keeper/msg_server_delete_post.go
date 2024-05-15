f0efc2dd720758aa259fe0806ff8f9ab6fac493d
oxFrost <cooliceblast0904@gmail.com>
2024-05-14 22:47:20 -0400

    test

--- x/blog/keeper/msg_server_delete_post.go
+++ x/blog/keeper/msg_server_delete_post.go
@@ -12,7 +12,7 @@ import (
 
 func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-	val, found := k.GetPost(ctx, msg.Id)
+	_, found := k.GetPost(ctx, msg.Id)
 
 	if !found {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
